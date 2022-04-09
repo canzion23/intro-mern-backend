cff8158532a222d8fe7941a35a90847d8558efcf
Luis Geovanni García <canzion23@gmail.com>
2022-04-09 00:33:54 -0600

    Actualización de avance en tutorial Developero

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const productRoutes = require('./routes/product')
 
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use('/public', express.static(`${__dirname}/storage/imgs`))
 
 app.use('/v1', productRoutes )
 
--- controllers/productController.js
+++ controllers/productController.js
@@ -1,7 +1,48 @@
-function addProduct (req, res){
-    res.status(201).send({ success: true})
+const Product = require('../models/Product') 
+
+// Agregar  productos a la base de datos
+async function addProduct (req, res){
+    try {
+
+        const {
+            name, 
+            size,
+            unitaryPrice,
+            description
+        } = req.body        
+
+        const product = Product({
+            name,
+            size,
+            unitaryPrice,
+            description
+        })
+
+        if(req.file) {
+            const { filename } = req.file
+            console.log('Nombre del archivo: '+filename);
+            product.setImgUrl(filename)
+        }
+
+        const productStored = await product.save()
+
+        res.status(201).send({ productStored })
+
+    } catch (e) {
+        res.status(500).send({ message: e.message})
+    }
+}
+
+
+// Obtener productos de la base de datos
+async function getProducts(req, res){
+
+    const products = await Product.  find().lean().exec()
+    res.status(200).send({products})
+
 }
 
 module.exports = {
-    addProduct
+    addProduct, 
+    getProducts
 }
