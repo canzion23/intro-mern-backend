90b755f7234334ef8450bc75ce795cee643b8515
Luis Geovanni García <canzion23@gmail.com>
2022-04-13 14:49:02 -0600

    Terminación del tutorial intro-mern

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-require('dotenv').config()
-const app = require('./app')
-const connectDb = require('./db/mongodb')
-const { appConfig, dbConfig } = require('./config')
+require('dotenv').config();
+const app = require('./app');
+const connectDb = require('./db/mongodb');
+const { appConfig, dbConfig } = require('./config');
 
-async function initApp (appConfig, dbConfig) {
-    try {
-        await connectDb(dbConfig)
-        app.listen(appConfig.port, () => console.log(`Listen in port: ${appConfig.port}`))        
-    } catch (e) {
-        console.error(e)
-        process.exit(0);
-    }    
+async function initApp(appConfig, dbConfig) {
+  try {
+    await connectDb(dbConfig);
+    app.listen(appConfig.port, () =>
+      console.log(`Listen in port: ${appConfig.port}`)
+    );
+  } catch (e) {
+    console.error(e);
+    process.exit(0);
+  }
 }
 
-initApp(appConfig, dbConfig)
+initApp(appConfig, dbConfig);
